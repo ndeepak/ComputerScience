f7b9ec09581b414de46fe2854957a5797317d8d4
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-05-27 19:12:44 +0545

    Create ToggleTheString.cpp
    Toggle the Alphabets cases

---
+++ DataStructure&Algorithm/String/ToggleTheString.cpp
@@ -0,0 +1,18 @@
+//Toggle the Alphabets cases
+// i.e. upper case to lower case and vice versa
+#include<iostream>
+using namespace std;
+int main(){
+	
+	char A[]={"HeLlOwOrLd"};
+	int i;
+	for(i=0;A[i]!='\0';i++){
+		if(A[i]>=65 && A[i]<=90){
+			A[i]+=32;
+		}
+		else if(A[i]>='a' && A[i]<=122 ){
+			A[i]-=32;
+		}
+	}
+	cout<<A;
+}
