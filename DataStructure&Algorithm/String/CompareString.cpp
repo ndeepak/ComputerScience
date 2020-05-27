7aa786541bcd95180aff8668b48d8173079084e8
Nagarkotideepak <45748958+Nagarkotideepak@users.noreply.github.com>
2020-05-27 21:01:54 +0545

    Create CompareString.cpp
    Comparing Strings

---
+++ DataStructure&Algorithm/String/CompareString.cpp
@@ -0,0 +1,24 @@
+//Comparing Strings
+#include<iostream>
+using namespace std;
+int main(){
+	
+	char A[]="Nepalese";
+	char B[]="Nepale";
+	int i,j;
+	for(i=0,j=0;A[i]!='\0' && B[i]!='\0';i++,j++){
+		if(A[i]!=B[j]){
+			break;
+		}
+	}
+	if(A[i]==B[j]){
+		cout<<"Equal";
+	}
+	else if(A[i]<B[j]){
+		cout<<"Smaller";
+	}
+	else{
+		cout<<"Greater";
+	}
+	return 0;
+}
